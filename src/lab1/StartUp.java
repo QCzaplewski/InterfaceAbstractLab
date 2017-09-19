d8697a854695f38c06c23d4e64bc5b97d141a4af
Quinten <Quinten@192.168.50.1>
2017-09-19 11:23:26 -0500

    Lab1: Created the StartUp class with three objects for each course.

--- src/lab1/StartUp.java
+++ src/lab1/StartUp.java
@@ -11,6 +11,18 @@ package lab1;
  */
 public class StartUp {
     public static void main(String[] args) {
+        Course course1 = new IntroToProgrammingCourse("Intro To Programming","111-111", 1.0,"None");
+        Course course2 = new IntroJavaCourse("Intro Java","222-222", 2.0,"Intro to Java Programming");
+        Course course3 = new AdvancedJavaCourse("Advanced Java","333-333", 3.0,"Intro Java");
+        
+        Course[] courses = {course1, course2, course3};
+        
+        for(Course course : courses){
+            System.out.println(course.getCourseName() + ": "
+                    + "\n\tCourse Number: " + course.getCourseNumber() 
+                    + "\n\tCredits: " + course.getCredits() 
+                    + "\n\tPrerequisites: " + course.getPrerequisites());
+        }
         
     }
 }
